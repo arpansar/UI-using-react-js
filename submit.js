81b870baa103d8ed9f385d0f9444cdc8fa2bcf75
arpansar <44233765+arpansar@users.noreply.github.com>
2018-10-17 19:49:07 +0530

    submit file

---
+++ submit.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Successful Submission</h3>
+      </div>
+    );
+  }
+}
